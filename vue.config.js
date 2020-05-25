46c43fbc08f3ebc2416e0aeab3d49aae01cd35ca
maxiaoqu <maxiaoqu@163.com>
2020-05-25 21:02:06 +0800

    添加npm run build --report检查包问题

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,18 @@ module.exports = {
   outputDir: process.env.outputDir,
   productionSourceMap: false,
   filenameHashing: false,
+  // 添加打包分析工具,使用方法：npm run build --report
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      if (process.env.npm_config_report) {
+        config
+          .plugin('webpack-bundle-analyzer')
+          .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+          .end()
+        config.plugins.delete('prefetch')
+      }
+    }
+  },
   devServer: {
     port: 1024,
     host: 'localhost',
