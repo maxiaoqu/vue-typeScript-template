1980fc3e425a480e5dba4ca68495d361bc56a402
maxiaoqu <maxiaoqu@163.com>
2020-06-02 07:29:40 +0800

    移动文件夹位置！

---
+++ src/frameSkeleton/layout/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout home">
+    <h4 style="color: blueviolet;">
+      【后台管理系统】这里是公用菜单、头部
+    </h4>
+    <main class="layout-box">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'Home'
+})
+export default class Home extends Vue {
+
+}
+</script>
