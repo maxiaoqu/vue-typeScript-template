ab28e21343feacbd1a44157dbe8b0347ec42503a
maxiaoqu <maxiaoqu@163.com>
2020-06-05 07:52:23 +0800

    修改状态管理

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { AppState } from './modules/app'
 import { UserState } from './modules/user'
-import { OidcState } from './modules/oidc'
-
-Vue.use(Vuex)
 
 Vue.use(Vuex)
 
 export interface RootState {
   app: AppState
   user: UserState
-  oidc: OidcState
 }
 
 export default new Vuex.Store<RootState>({})
