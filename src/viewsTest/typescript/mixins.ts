6588a9e8e236d2bbc6ff63d84c8fc3e20a76a0c9
maxiaoqu <maxiaoqu@163.com>
2020-06-14 16:29:33 +0800

    修改Mixins的方法

--- src/viewsTest/typescript/mixins.ts
+++ src/viewsTest/typescript/mixins.ts
@@ -1,26 +1,25 @@
-import {Component, Prop, Vue} from 'vue-property-decorator'
-
+import { Component, Prop, Vue } from 'vue-property-decorator'
+// 声明全局方法
 declare module 'vue/types/vue' {
   interface Vue {
-    form: Object
+    testNameArr: string[],
+    initEcharts(): void,
+    initEcharts2(): void
   }
 }
-
 /**
  * mixins
  */
 @Component<MyMixins>({
   name: 'MyMixins'
 })
-
 export default class MyMixins extends Vue {
   /* props */
   @Prop(Boolean) public testPropData: boolean
-  @Prop({type: String, default: ''}) public testPropData2!: string
-
+  @Prop({ type: String, default: '' }) public testPropData2!: string
   /* data() */
   // 注意：只能使用public定义，不能使用protected、定义，否则组件内收不到
-  public testNameArr: string[] = []
+  testNameArr: string[] = []
 
   /*
   类似：vue的生命周期
@@ -33,11 +32,11 @@ export default class MyMixins extends Vue {
   类似：methods
   注意：在本脚本调用的使用private，在组件内调用的，使用public
   */
-  private initEcharts() {
+  initEcharts(): void {
     this.testNameArr = ['1', '2']
   }
 
-  public initEcharts2() {
-    console.info('methods')
+  initEcharts2(): void {
+    console.log('methods', this.$refs.UseMixins)
   }
 }
--- src/viewsTest/useMixins.vue
+++ src/viewsTest/useMixins.vue
@@ -2,25 +2,28 @@
 说明：使用Mixins方式的案例页面
 -->
 <template>
-  <div class="UseMixins"/>
+  <div
+    ref="UseMixins"
+    class="UseMixins"
+  />
 </template>
 
 <script lang="ts">
-  import {Component, Mixins} from 'vue-property-decorator'
-  import MyMixins from '@/viewsTest/typescript/mixins'
+import { Component, Mixins } from 'vue-property-decorator'
+import MyMixins from '@/viewsTest/typescript/mixins'
 
   @Component<UseMixins>({
     name: 'UseMixins'
   })
-  export default class UseMixins extends Mixins(MyMixins) {
-    mounted() {
-      console.log(this.testNameArr, this.testPropData, this.testPropData2)
-      console.log(this.initEcharts2())
-    }
+export default class UseMixins extends Mixins(MyMixins) {
+  mounted() {
+    console.log(1111, this.testNameArr, this.testPropData, this.testPropData2)
+    this.initEcharts2()
   }
+}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .UseMixins {
     width: 100%;
     height: 100%;
