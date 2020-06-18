95e1068a584b912d9b145c5d0cdb1d857e662188
maxiaoqu <maxiaoqu@163.com>
2020-06-18 22:05:39 +0800

    处理路由再次点击报错的bug

--- src/router/setRouter.ts
+++ src/router/setRouter.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig, RouterOptions } from 'vue-router'
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 Vue.use(VueRouter)
 
 const startRouter: RouteConfig[] = []
