94b4d5f9ae17acba162d72938158b0232612767d
maxiaoqu <maxiaoqu@163.com>
2020-06-25 22:33:34 +0800

    修改登录页和oidc的页面

--- src/frameSkeleton/oidc/OidcCallback.vue
+++ src/frameSkeleton/oidc/OidcCallback.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="OidcCallback" />
+  <div class="OidcCallback">
+    <div class="scene">
+      <svg
+        id="dc-spinner"
+        version="1.1"
+        x="240px"
+        y="240px"
+        width="240"
+        height="240"
+        viewBox="0 0 240 240"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMinYMin meet"
+      >
+        <text
+          x="60"
+          y="120"
+          font-family="Monaco"
+          font-size="24px"
+          style="letter-spacing:0.6"
+          fill="grey"
+        >
+          进入系统...
+          <animate
+            attributeName="opacity"
+            values="0;1;0"
+            dur="1.8s"
+            repeatCount="indefinite"
+          />
+        </text>
+      </svg>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -19,16 +50,23 @@ export default class OidcCallback extends Vue {
 
 <style lang="scss" scoped>
   .OidcCallback {
-    background: #ff7e5f;
-    background: -webkit-linear-gradient(to right, #feb47b, #ff7e5f);
-    background: linear-gradient(to right, #feb47b, #ff7e5f);
+    width: 100%;
     height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: 0;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
+    text-size-adjust: 100%;
+    background: #2f3242;
+    background-size: 163px;
+
+    .scene {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+        width: 240px;
+        height: 240px;
+      }
+    }
   }
 </style>
