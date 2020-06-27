e5f6c406e2df57eb61e7c1cc4e889f405173b055
maxiaoqu <maxiaoqu@163.com>
2020-06-27 20:54:25 +0800

    删除没必要的文件

--- src/plugin/echarts/mixins/echartsComponent.ts
+++
@@ -1,6 +0,0 @@
-// 引入基础的echarts组件
-require('echarts/lib/component/title')
-require('echarts/lib/component/toolbox')
-require('echarts/lib/component/tooltip')
-require('echarts/lib/component/grid')
-require('echarts/lib/component/legend')
--- src/plugin/echarts/multiLineEcharts.ts
+++
@@ -1,9 +0,0 @@
-// 引入 ECharts 主模块
-var multiLineEcharts = require('echarts/lib/echarts')
-// 引入柱状图等
-require('echarts/lib/chart/line')
-
-// 引入echarts(提示框、标题)等组件
-require('../mixins/echartsComponent')
-
-export default multiLineEcharts
