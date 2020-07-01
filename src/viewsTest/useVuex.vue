edb17e2be6b4b32cd6ace72c709ce0bfec32d16b
maxiaoqu <maxiaoqu@163.com>
2020-07-01 21:05:58 +0800

    useVuex

---
+++ src/viewsTest/useVuex.vue
@@ -0,0 +1,26 @@
+<!--
+说明：Vuex使用案例页面
+-->
+<template>
+  <div class="useVuex">
+    Vuex使用
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+  @Component<useVuex>({
+    name: 'useVuex'
+  })
+export default class useVuex extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+  .useVuex {
+    width: 100%;
+    height: 100%;
+  }
+</style>
