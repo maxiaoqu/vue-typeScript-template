6a78adaa6ad6a0741b90866ad1a6a8ad6b7416d1
maxiaoqu <maxiaoqu@163.com>
2020-07-04 20:33:16 +0800

    修改主要文件的别名配置

--- src/environment/baseUrl.ts
+++ src/environment/baseUrl.ts
@@ -2,7 +2,7 @@
  * @description 配置线上环境的相关接口地址
  */
 
-import { isProduction } from '@/environment/process'
+import { isProduction } from './process'
 
 // 运行环境【运行后台人员联调环境】----【运行：npm run dev】
 const development = {
