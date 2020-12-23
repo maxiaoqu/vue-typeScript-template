ce0da46d14fd8b6f6fc4f2edc70b2906619c80ab
maxiaoqu <maxiaoqu@163.com>
2020-12-23 08:44:54 +0800

    修复ooidc

--- src/plugins/echarts/mixins/echartsTool.ts
+++
@@ -1,9 +0,0 @@
-import Echarts from 'echarts'
-
-export const getEchartsTool = (toolName) => {
-  let echartsTool = Echarts
-  if (toolName) {
-    echartsTool = Echarts[toolName]
-  }
-  return echartsTool
-}
